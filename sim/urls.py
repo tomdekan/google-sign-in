4a4bb17b164eea8b7e52de6c3226c452f2941a3d
a-toms <1239@pm.me>
2024-07-19 12:57:56 +0100

    Fixed login issue.

---
+++ sim/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.sign_in, name='sign_in'),
+    path('sign-out', views.sign_out, name='sign_out'),
+    path('auth-receiver', views.auth_receiver, name='auth_receiver'),
+]
